72bf92929a86c559bdd423aae677aead5ab47a11
Pchel666 <pchel66694@gmail.com>
2018-11-04 15:32:59 -0500

    SJF done, now testing

--- src/process/scheduling/Scheduler.java
+++ src/process/scheduling/Scheduler.java
@@ -1,5 +1,6 @@
 package process.scheduling;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Scheduler 
@@ -74,15 +75,40 @@ public class Scheduler
 				processes[i].setPriority(holder);
 			}
 		}
-		
-	//choosing algorithm
-	//TODO:choose algorithm, make method for each algorithm, make the chart
+		//choosing algorithm
+		String alg = "";
+		while(!alg.equals("FCFS")&&!alg.equals("SJF")&&!alg.equals("SRT")&&!alg.equals("Priority")&&!alg.equals("RRf")&&!alg.equals("RRv"))
+		{
+			System.out.println("What algorithm to use?(FCFS, SJF, SRT, Priority, RRf, RRv)");
+			alg = reader.nextLine();
+		}
+		switch(alg)
+		{
+			case "FCFS":
+				//FCFS
+				break;
+			case "SJF":
+				SJF(processes);
+				break;
+			case "SRT":
+				//SRT
+				break;
+			case "Priority":
+				//Priority
+				break;
+			case "RRf":
+				//RRf
+				break;
+			case "RRv":
+				//RRv
+				break;
+		}
 	}
 	
-	private double[] FCFS(Process[] process, int length)
+	private static double[] FCFS(Process[] process, int length)
 	{
 		//first come first serve - non-preemptive
-		double[] results = {};
+		double[] results = {}; //{wait time, turnaround time}
 		double wt= 0;
 		double tat= 0;
 		
@@ -105,58 +131,97 @@ public class Scheduler
 		return results;
 	}
 	
-	private double[] SJF(Process[] process)
+	private static double[] SJF(Process[] process)
 	{
 		//short job first - non-preemptive
-		Process[] reorgProcess = new Process[process.length];		
-		double[] results = {0,0};
-		double[] wt= new double[process.length];
-		double[] tat= new double[process.length];
-		int counter = 0;
-		int scounter = 0;
-		while (counter<process.length)
+		Process[] reorgProcess = new Process[process.length]; //for reorganizing by arrival time
+		ArrayList<Process> tempList = new ArrayList<Process>(0);
+		double[] results = {0,0}; //{wait time, turnaround time}
+		int counter = 0; //reorganized array position counter
+		int scounter = 0; //arrival time counter
+		int bcounter = 0; //burst time counter
+		int maxb = 0; //maximum burst time
+		for(int i=0;i<process.length;i++) //getting maximum burst time
 		{
-			for(int i=0;i<process.length;i++)
+			if(process[i].getBursttime()>maxb)
+			{
+				maxb=process[i].getBursttime();
+			}
+		}
+		while (counter<process.length) //reorganizing by arrival and burst times
+		{
+			for(int i=0;i<process.length;i++) //creates temporary arraylist with all processes with arrival time = scounter
 			{
 				if (process[i].getArrivaltime()==scounter)
 				{
-					reorgProcess[counter]=process[i];
-					counter++;
+					tempList.add(process[i]);
 				}
 			}
 			scounter++;
+			while(bcounter<=maxb) //sorts the temporary arraylist processes by burst time and puts them in the reorganized processes array
+			{
+				for(int i=0;i<tempList.size();i++)
+				{
+					if(tempList.get(i).getBursttime()==bcounter)
+					{
+						reorgProcess[counter]=tempList.get(i);
+						counter++;
+					}
+				}
+				bcounter++;
+			}
+			tempList.clear();
+			bcounter=0;
 		}
+		int ct = 0;
+		for(int i=0;i<reorgProcess.length;i++)
+		{
+			if(ct<=reorgProcess[i].getArrivaltime())
+			{
+				ct=reorgProcess[i].getArrivaltime()+reorgProcess[i].getBursttime();
+				results[1]+=ct-reorgProcess[i].getArrivaltime();
+				results[0]+=results[1]-reorgProcess[i].getBursttime();
+			}
+			else
+			{
+				ct=ct+reorgProcess[i].getBursttime();
+				results[1]+=ct-reorgProcess[i].getArrivaltime();
+				results[0]+=results[1]-reorgProcess[i].getBursttime();
+			}
+		}
+		results[0]=results[0]/reorgProcess.length;
+		results[1]=results[1]/reorgProcess.length;
 		return results;
 	}
 	
-	private double[] SRT(Process[] process)
+	private static double[] SRT(Process[] process)
 	{
 		//shortest remaining time - preemptive
-		double[] results = {0,0};
+		double[] results = {0,0}; //{wait time, turnaround time}
 		
 		return results;
 	}
 	
-	private double[] Priority(Process[] process)
+	private static double[] Priority(Process[] process)
 	{
 		//priority - preemptive
-		double[] results = {0,0};
+		double[] results = {0,0}; //{wait time, turnaround time}
 		
 		return results;
 	}
 	
-	private double[] RRf(Process[] process)
+	private static double[] RRf(Process[] process)
 	{
 		//round robin (fixed)
-		double[] results = {0,0};
+		double[] results = {0,0}; //{wait time, turnaround time}
 		
 		return results;
 	}
 	
-	private double[] RRv(Process[] process)
+	private static double[] RRv(Process[] process)
 	{
 		//round robin (variable)
-		double[] results = {0,0};
+		double[] results = {0,0}; //{wait time, turnaround time}
 		
 		return results;
 	}
